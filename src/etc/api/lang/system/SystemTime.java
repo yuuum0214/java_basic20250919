bccd541b0ea4d984520514ed8aa4976c6f05701b
yumi Kim <cdcd0214@gmail.com>
2025-09-24 18:28:44 +0900

    currentTimeMillis

---
+++ src/etc/api/lang/system/SystemTime.java
@@ -0,0 +1,19 @@
+package etc.api.lang.system;
+
+public class SystemTime {
+    public static void main(String[] args) {
+
+        long start = System.currentTimeMillis();
+
+        int total = 0;
+        for (int i =0; i < 2000000000; i++){
+            total += i;
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("실행 소요시간: " + (end - start) * 0.001 + "초" );
+        System.out.println(System.getProperties());
+
+    }
+}
